44468083a09261aa9e4601a4ff21e776e0fd2850
user <126427971+theantigone@users.noreply.github.com>
2025-06-10 14:01:49 -0400

    Implement test project for agentic ai

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
 
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv  # returns a boolean (True/False)
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
     user_prompt = " ".join(args)
@@ -26,22 +27,22 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    generate_content(client, messages)
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    generate_content(client, messages, verbose)
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
-    if sys.argv[2:]:
-        if sys.argv[2] == "--verbose":
-            print("User prompt:", response.text)
-            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-            print("Response tokens:", response.usage_metadata.candidates_token_count)
-    else:
-        print("Response:")
-        print(response.text)
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
