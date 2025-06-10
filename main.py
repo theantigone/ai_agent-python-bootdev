e520d21e910a57c1326d912228634ce10bb741f3
user <126427971+theantigone@users.noreply.github.com>
2025-06-10 01:20:51 -0400

    Softcode input into the Gemini API

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,10 +9,11 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-response=client.models.generate_content(
-    model='gemini-2.0-flash-001',contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=sys.argv[1],
 )
 print(response.text)
 
-print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
