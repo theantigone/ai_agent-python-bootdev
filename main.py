e79994a023bcfbbb4587ec117536e9ba61b574b4
user <126427971+theantigone@users.noreply.github.com>
2025-06-10 01:48:53 -0400

    Add `--verbose` argument into the agentic AI

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
-import os
 import sys
+import os
+
+from google import genai
+from google.genai import types
 
 from dotenv import load_dotenv
-from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=sys.argv[1],
-)
-print(response.text)
+    args = sys.argv[1:]
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    if sys.argv[2:]:
+        if sys.argv[2] == "--verbose":
+            print("User prompt:", response.text)
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
+    else:
+        print("Response:")
+        print(response.text)
+
+
+if __name__ == "__main__":
+    main()
