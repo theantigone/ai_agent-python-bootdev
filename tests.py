2261bc3faa482ea8ca6fffb5ac6f64b9020e1024
Quang Hoang <126427971+theantigone@users.noreply.github.com>
2025-08-25 02:46:17 -0400

    Make the agent run Python code

--- tests.py
+++ tests.py
@@ -1,53 +1,15 @@
 # from functions.get_files_info import get_files_info
 # from functions.get_file_content import get_file_content
-from functions.write_file import write_file
+# from functions.write_file_content import write_file
+from functions.run_python import run_python_file
 
 
 def test():
-    #    result = get_files_info("calculator", ".")
-    #    print("Result for current directory:")
-    #    print(result)
-    #    print("")
-    #
-    #    result = get_files_info("calculator", "pkg")
-    #    print("Result for 'pkg' directory:")
-    #    print(result)
-    #
-    #    # NOTE: this should return an error string
-    #    result = get_files_info("calculator", "/bin")
-    #    print("Result for '/bin' directory:")
-    #    print(result)
-    #
-    #    # NOTE: this should return an error string
-    #    result = get_files_info("calculator", "../")
-    #    print("Result for '../' directory:")
-    #    print(result)
-    #
-    #    result = get_file_content("calculator", "lorem.txt")
-    #    print("Result for lorem.txt:")
-    #    print(result)
-
-    #    result = get_file_content("calculator", "main.py")
-    #    print("Result for main.py:")
-    #    print(result)
-    #
-    #    result = get_file_content("calculator", "pkg/calculator.py")
-    #    print("Result for pkg/calculator.py:")
-    #    print(result)
-    #
-    #    # NOTE: this should return an error string
-    #    result = get_file_content("calculator", "/bin/cat")
-    #    print("Result for /bin/cat:")
-    #    print(result)
-
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print(result)
-
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print(result)
-
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print(result)
+    print(run_python_file("calculator", "main.py"))
+    print(run_python_file("calculator", "main.py", ["3 + 5"]))
+    print(run_python_file("calculator", "tests.py"))
+    print(run_python_file("calculator", "../main.py"))
+    print(run_python_file("calculator", "nonexistent.py"))
 
 
 if __name__ == "__main__":
